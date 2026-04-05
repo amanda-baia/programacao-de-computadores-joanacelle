4547bad859367f1ab83eb47b8c922bcb762bf2b5
amanda-baia <amandabaiaadv@gmail.com>
2026-04-04 22:30:41 -0300

    Add files via upload

---
+++ c) Construa um algoritmo que receba o valor de um produto e calcule o preço final considerando um acréstimo de 8 por cento de imposto.py
@@ -0,0 +1,5 @@
+# c) Construa um algoritmo que receba o valor de um produto e calcule o preço final considerando um acréstimo de 8 \por cento de imposto:
+
+p = float(input('digite o valor de um número produto'))
+vf = p + (p * 8/100)
+print ("O valor final do produto, acrescido do valor do imposto é:", vf)
