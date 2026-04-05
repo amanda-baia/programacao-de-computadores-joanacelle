11e64e35e603129c8ab159367529383ff843c137
amanda-baia <amandabaiaadv@gmail.com>
2026-04-04 22:54:57 -0300

    Delete f) Elabore um algoritmo que receba a temperatura em graus Celsius e apresente o valor convertido para graus Fahrenheit.py

--- f) Elabore um algoritmo que receba a temperatura em graus Celsius e apresente o valor convertido para graus Fahrenheit.py
+++
@@ -1,6 +0,0 @@
-#f) Elabore um algoritmo que receba a temperatura em graus Celsius e apresente o valor convertido para graus Fahrenheit:
-
-t = float(input('digite a temperatura em graus Celsius'))
-F = (t*1.8)+32
-
-print ("O valor é:", F, "ºF")
