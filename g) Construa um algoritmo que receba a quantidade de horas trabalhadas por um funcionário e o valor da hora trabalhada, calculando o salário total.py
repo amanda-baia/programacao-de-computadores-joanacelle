6f3b81a8f8b613069d20860b211a7a42b881b2cf
amanda-baia <amandabaiaadv@gmail.com>
2026-04-04 22:55:00 -0300

    Delete g) Construa um algoritmo que receba a quantidade de horas trabalhadas por um funcionário e o valor da hora trabalhada, calculando o salário total.py

--- g) Construa um algoritmo que receba a quantidade de horas trabalhadas por um funcionário e o valor da hora trabalhada, calculando o salário total.py
+++
@@ -1,8 +0,0 @@
-#g) Construa um algoritmo que receba a quantidade de horas trabalhadas por um funcionário e o valor da hora trabalhada, calculando o salário total.
-
-Ht = float(input('digite o valor da hora trabalhada'))
-V = float(input('digite o valor da hora trabalhada'))
-
-S = Ht * V
-print ("O valor da hora trabalhada em R$ é:", S)
-
